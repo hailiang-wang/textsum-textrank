fad266458b4afc14119bebc9cefd6ed076a52033
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-11-10 15:58:39 +0800

    load evaluate json

---
+++ scripts/client.start.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+cd client
+superstatic --port 8101 --host 0.0.0.0
